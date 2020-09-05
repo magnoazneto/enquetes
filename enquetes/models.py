546988c24373b6b02cb350253d5b7719ac9df08d
magnoazneto <magnoazneto@gmail.com>
2020-09-05 15:50:04 -0300

    alteracao no model de enquetes para usar o banco de dados

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-# Create your models here.
-   
 
+class Enquete(models.Model):
+    texto = models.CharField(max_length=40)
+    data_publicacao = models.DateField(max_length=20)
+
+
+"""
 class Enquete(object):
     def __init__(self, id=0, texto='', data_publicacao=''):
         self.id = id
         self.texto = texto
         self.data_publicacao = data_publicacao
+"""
 
 
--- enquetes/views.py
+++ enquetes/views.py
@@ -17,16 +17,8 @@ def exibir(request, perfil_id):
     return render(request, 'perfil.html', {'perfil': perfil})
 
 def get_enquete(request, enquete_id):
-    enquete = Enquete()
-    if enquete_id == 1:
-        print('here')
-        enquete = Enquete(enquete_id, 'Qual o framework web mais famoso da stack javascript?',
-                            '12-02-2020')
-    if enquete_id == 2:
-        enquete = Enquete(enquete_id, 'Qual o framework para desenvolvimento híbrido mais recente da google?',
-                            '15-06-2020')
-    if enquete_id == 3:
-        enquete = Enquete(enquete_id, 'Onde você mora?',
-                            '19-08-2020')
-    print(enquete.texto)
+    enquete = Enquete.objects.get(id=enquete_id)
     return render(request, 'enquete.html', {'enquete': enquete})
+    
+   
+ 
